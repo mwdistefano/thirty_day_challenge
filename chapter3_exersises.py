583e85fd9e72da043549a71595edd4dbc318c771
mwdistefano <mwdistefano@gmail.com>
2013-03-04 17:35:26 -0500

    30 day challenge

---
+++ chapter3_exersises.py
@@ -0,0 +1,14 @@
+# Exercise 3.1
+# Traceback (most recent call last):
+# File "<stdin>", line 1, in <module>
+# NameError: name 'repeat_lyrics' is not defined
+
+# Exercise 3.2
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   File "<stdin>", line 2, in repeat_lyrics
+# NameError: global name 'print_lyrics' is not defined
+
+# Exercise 3.3
+# allen = '' + 'allen'
+# allen = 'allen'.rjust(70)git 
