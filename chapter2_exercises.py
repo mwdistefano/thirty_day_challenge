163bddff0abd3c1c1fdafd82e9aa82e0a03be874
mwdistefano <mwdistefano@gmail.com>
2013-03-04 16:17:56 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,31 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Matthew DiStefano
+# Problem 2.1
+# To display 01, 010, 0100, and 01000, they would be displayed like this:
+# >>> '01'
+# >>> '010'
+# >>> '0100'
+# >>> '01000'
+# there needs to be a quote around numbers because they are a sting, not and integer.
+
+# Problem 2.2
+#print 1
+#x = 2
+#print x
+#5
+#x = 5
+
+# nothing happens because we are not pushing. In the terminal, x + 5 = 6. 
+
+# Problem 2.3
+# 1: 8
+# 2: 8.5
+# 3: 4.0
+# 4: 11
+# 5: '.....'
+
+# Problem 2.4
+# 1: 523.3333333333334
+# 2: 945.4499999999999
+# 3: 7:30:06 AM
